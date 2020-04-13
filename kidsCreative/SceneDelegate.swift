b8f361844f0e24bf8feb2d0b470f780cb106e2dd
howardC56 <h2chang56@gmail.com>
2020-04-13 19:59:42 -0400

    init setup

--- kidsCreative/SceneDelegate.swift
+++ kidsCreative/SceneDelegate.swift
@@ -15,9 +15,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-                window = UIWindow(frame: UIScreen.main.bounds)
-                window?.windowScene = windowScene
-                window?.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController:MainViewController())
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
