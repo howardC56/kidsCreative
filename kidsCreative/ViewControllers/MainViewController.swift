79ded5e1e99fbb06ef934d6ed936329966b92b1b
Margiett Gil <mxg627@icloud.com>
2020-04-15 13:30:14 -0400

    added the extensions for collectionview delegate and datasource

--- kidsCreative/ViewControllers/MainViewController.swift
+++ kidsCreative/ViewControllers/MainViewController.swift
@@ -19,8 +19,38 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        mainView.collectionView.dataSource = self
+        mainView.collectionView.delegate = self
+        mainView.collectionView.register(MainViewCell.self, forCellWithReuseIdentifier: "mainViewCell")
     }
 
 
+}
+
+//MARK: UICollectionView DataSource Extension
+extension MainView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mainViewCell", for: indexPath) as? MainViewCell else {
+            fatalError("could not downcast to MainViewCell")
+        }
+        return cell
+    }
+}
+//MARK: UICollectionView Delegate Extension
+extension MainView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxSize.width
+        let itemHeight: CGFloat = maxSize.height * 0.75 // 75%
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
 }
 
