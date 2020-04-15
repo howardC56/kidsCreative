bd1f08a45441b0401c6895f939145e8a1365e130
Margiett Gil <mxg627@icloud.com>
2020-04-14 23:07:07 -0400

    added the labels and images to the collectionview cell but for some reason is not loading figuring out my constraints

---
+++ kidsCreative/Views/MainViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewCell.swift
+//  kidsCreative
+//
+//  Created by Margiett Gil on 4/14/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import UIKit
+
+class MainViewCell: UICollectionViewCell {
+    
+}
